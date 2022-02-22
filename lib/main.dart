5e7a6db1afb2e7ff919de1886cdd8541a32c981f
marscj <mars.jinxing@gmail.com>
2022-02-22 14:17:24 +0400

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -23,8 +23,10 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-        title: "Application",
-        initialRoute: AppPages.INITIAL,
-        getPages: AppPages.routes);
+      title: "Application",
+      initialRoute: AppPages.INITIAL,
+      getPages: AppPages.routes,
+      theme: ThemeData(fontFamily: 'NotoSansSC'),
+    );
   }
 }
